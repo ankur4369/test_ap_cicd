b632163313a7bcd85f55816a968db8b346812144
Ankur Pandey <zsankur8289@gmail.com>
2025-08-01 14:06:31 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,5 +5,18 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Name of S3 bucket"
   type = string
-  default = "my-github-s3-bucket-082025"
+  default = "my-github-s3-glue-bucket-082025"
+}
+
+variable "job_name" {
+  default = "my_first_glue_job_ap_082025"
+}
+
+variable "iam_role_arn" {
+  type = string
+  description = "IAM Role ARN for AWS Glue"
+}
+
+variable "script_path" {
+  default = "glue_job_script.py"
 }
