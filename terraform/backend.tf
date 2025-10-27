83b243c4497883a1b3b30dfc2499d07137bf7697
Ankur Pandey <zsankur8289@gmail.com>
2025-10-27 12:56:18 +0000

    Add the SSH key pair to AWS 7

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "cpc-ankurp-tf-bucket"   # Name from above
     key            = "terraform.tfstate"  # Path within the bucket
-    region         = var.aws_region
-    dynamodb_table = "terraform-lock-table"
+    region         = "us-east-1"
+    use_lockfile   = true
     encrypt        = true
   }
 }
