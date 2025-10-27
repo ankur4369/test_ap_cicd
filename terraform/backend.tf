a149ece11826516f54ad0ec49a33b39982d76089
Ankur Pandey <zsankur8289@gmail.com>
2025-10-27 16:06:45 +0000

    Add Glue Script 5

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "cpc-ankurp-tf-bucket-1"
+        bucket = "cpc-ankurp-tf-bucket"
         key = "terraform.tfstate"
         region = "us-east-1"
     }
